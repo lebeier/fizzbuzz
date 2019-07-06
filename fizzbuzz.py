d9992e4c47aff0ec4a5e90ac9452640f6205dd31
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-07-06 11:35:29 +0800

    Modify test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,15 @@
 class FizzBuzz(object):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def run(self, num):
-    if num == 3:
-      return 'Fizz'
-    elif num == 5:
-      return 'Buzz'
-    elif num == 15:
-      return 'FizzBuzz'
-    return num
+    def run(self, num):
+        if num == 3:
+            return 'Fizz'
+        elif num == 15:
+            return 'FizzBuzz'
+        elif num % 5 == 0:
+            return 'Buzz'
+        return num
+
+    def run_sequence(self, max):
+        return [self.run(i) for i in range(max)]
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,27 +1,31 @@
 from fizzbuzz import *
 
+
 def test_should_return_one():
-  fizzbuzz= FizzBuzz()
-  assert fizzbuzz.run(1) == 1
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run(1) == 1
+
 
 def test_should_return_two():
-  fizzbuzz= FizzBuzz()
-  assert fizzbuzz.run(2) == 2
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run(2) == 2
+
 
 def test_should_return_Fizz():
-  fizzbuzz= FizzBuzz()
-  assert fizzbuzz.run(3) == "Fizz"
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run(3) == "Fizz"
+
 
 def test_should_return_Buzz():
-  fizzbuzz= FizzBuzz()
-  assert fizzbuzz.run(5) == "Buzz"
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run(5) == "Buzz"
+
 
 def test_should_return_FizzBuzz():
-  fizzbuzz= FizzBuzz()
-  assert fizzbuzz.run(15) == "FizzBuzz"
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run(15) == "FizzBuzz"
+
 
 def test_should_return_10_Buzz():
-  fizzbuzz = FizzBuzz()
-  for i in range(10):
-    assert fizzbuzz.run(i*5) == "Buzz"
-
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.run_sequence(10) == ['FizzBuzz', 1, 2, 'Fizz', 4, "Buzz", 6, 7, 8, 9]
